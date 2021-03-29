4d4268afce6c35462110cb70aae8a9c64963650f
Phiphat Khammongkhon <57613198+Phiphat1609@users.noreply.github.com>
2021-03-29 13:01:33 +0600

    q13-2homework2-todolist
    q13-2homework2-todolist

---
+++ components/todolist/todolist.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent implements OnInit {
+
+  tasks: string[] = [];
+  task!: string;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  addTask() {
+    this.tasks.push(this.task);
+  }
+  
+}
